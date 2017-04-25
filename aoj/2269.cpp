841318781022da1c64095225ab843d008b81036c
Yuki Ito <acomagu@gmail.com>
2017-04-25 18:54:26 +0900

    [Add] implement aoj/2269

--- aoj/2269.cpp
+++ aoj/2269.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <map>
+#include <set>
 using namespace std;
 
 int main() {
+  map<set<int>, int> distances;
   int nPoint, nSide;
   cin >> nPoint >> nSide;
+  for(int iSide = 0; iSide < nSide; ++iSide) {
+    int iPoint1, iPoint2, distance;
+    cin >> iPoint1 >> iPoint2 >> distance;
+    distances[set<int>({iPoint1, iPoint2})] = 1;
+  }
+}
